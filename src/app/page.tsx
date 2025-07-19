8421cc370f35445e0321503da4ba757b27063dfe
Lavneesh Amit Sharma <lavneesh@Lavneeshs-MacBook-Air.local>
2025-07-19 22:22:28 +0530

    feat: integrate TRPC with React Query and add AI greeting functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { prisma } from '@/lib/db' // Adjust the import path as necessary
+import { useTRPC } from '@/trpc/client'
+import { text } from 'stream/consumers';
+import { trpc, getQueryClient } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Client } from './client';
 const page = async() => {
-  const users = await prisma.user.findMany();
+  const queryclient=getQueryClient();
+  void queryclient.prefetchQuery(trpc.createai.queryOptions({ text: 'Hello' }));
   return (
-    <div>
-     {JSON.stringify(users, null, 2)}
-    </div>
+    <HydrationBoundary state={dehydrate(queryclient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
