5958c9cbcf51afc2f8e3c513aa7d6b0ac40e1156
Lavneesh Amit Sharma <lavneesh@Lavneeshs-MacBook-Air.local>
2025-07-29 18:18:04 +0530

    feat: integrate theme provider and enhance messaging container with fragment handling and loading state

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -4,11 +4,14 @@ import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { ResizableHandle,ResizablePanel,ResizablePanelGroup } from "@/components/ui/resizable";
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 interface Props{
-    projectId:string;
+    projectId:string
 }
 export const ProjectView=({projectId}:Props)=>{
+    const [activeFragment,setActiveFragment]=useState<Fragment | null>(null)
     return(
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
@@ -16,9 +19,15 @@ export const ProjectView=({projectId}:Props)=>{
                 defaultSize={35}
                 minSize={20}
                 className="flex flex-col min-h-0">
-                    <Suspense fallback={<p>Loading messages...</p>}>
-                  <MessagesContainer projectId={projectId}/>
-                   </Suspense>
+                <Suspense fallback={<p>Loading messages...</p>}>
+                  <ProjectHeader projectId={projectId} />
+                </Suspense>
+                <Suspense fallback={<p>Loading messages...</p>}>
+                  <MessagesContainer 
+                  projectId={projectId}
+                  activeFragment={activeFragment}
+                  setActiveFragment={setActiveFragment}/>
+                </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
                 <ResizablePanel
